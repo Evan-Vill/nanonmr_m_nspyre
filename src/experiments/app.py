e8467ec8bec1b71e32e76cd6b90a9ee39cc8ad5f
Evan-Vill <ejvilla4@uchicago.edu>
2026-03-06 00:33:21 -0600

    Updated import files

--- src/experiments/app.py
+++ src/experiments/app.py
@@ -7,8 +7,7 @@ from pathlib import Path
 
 import nspyre.gui.widgets.save
 import nspyre.gui.widgets.load
-import nspyre.gui.widgets.flex_line_plot
-import nspyre.gui.widgets.subsystem
+# import nspyre.gui.widgets.subsystem
 from nspyre import MainWidget
 from nspyre import MainWidgetItem
 from nspyre import nspyre_init_logger
@@ -17,9 +16,8 @@ from nspyre import nspyreApp
 # in order for dynamic reloading of code to work, you must pass the specifc
 # module containing your class to MainWidgetItem, since the python reload()
 # function does not recursively reload modules
-import gui_test
 import gui_all_instruments_2026_03_02
-import gui_experiments_2026_03_02
+import gui_experiments_2026_03_03
 import gui_calculations
 
 from drivers.insmgr import MyInstrumentManager
@@ -43,18 +41,17 @@ def main():
         # Create the GUI.
         main_widget = MainWidget(
             {
-                # 'Test': MainWidgetItem(gui_test, 'PowerBar', stretch = (1, 1)),
                 'Instruments': MainWidgetItem(gui_all_instruments_2026_03_02, 'InstWidget', stretch = (1, 1)),
-                'Experiments': MainWidgetItem(gui_experiments_2026_03_02, 'ExpWidget', stretch = (1, 1)),
+                'Experiments': MainWidgetItem(gui_experiments_2026_03_03, 'ExpWidget', stretch = (1, 1)),
                 # 'Subsystems': MainWidgetItem(nspyre.gui.widgets.subsystem, 'SubsystemsWidget', args=[insmgr.subs.subsystems], stretch=(1, 1)),
                 'Plots': {
                     'FlexLinePlot': MainWidgetItem(
-                        gui_experiments_2026_03_02,
+                        gui_experiments_2026_03_03,
                         'FlexLinePlotWidgetAllDefaults',
                         stretch=(100, 100),
                     ),
                 },
-                'Fitter': MainWidgetItem(gui_experiments_2026_03_02, 'FitterWidget', stretch = (1, 1)),                
+                'Fitter': MainWidgetItem(gui_experiments_2026_03_03, 'FitterWidget', stretch = (1, 1)),                
                 'Calculations': MainWidgetItem(gui_calculations, 'CalcWidget', stretch = (1, 1)),
                 'Save': MainWidgetItem(nspyre.gui.widgets.save, 'SaveWidget', stretch=(1, 1)),
                 'Load': MainWidgetItem(nspyre.gui.widgets.load, 'LoadWidget', stretch=(1, 1)),
