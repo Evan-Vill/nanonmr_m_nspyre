5fac098ec11d9c9d8aaa94a1c8d78ac72b8d4f50
Evan Villafranca <ejvilla4@uchicago.edu>
2026-03-10 11:51:34 -0500

    Added AWG parameter control

--- src/experiments/gui_all_instruments_2026_03_02.py
+++ src/experiments/gui_all_instruments_2026_03_02.py
@@ -182,6 +182,7 @@ class InstWidget(QWidget):
         self.init_azi_widgets()
         self.init_sg396_widgets()
         self.init_laser_widgets()
+        self.init_awg_widgets()
         self.init_flipper_widgets()
         self.init_nd_filter_widgets()
         self.init_bpd_shutter_widgets()
@@ -851,6 +852,90 @@ class InstWidget(QWidget):
         )
         self.ps_status_label.setFixedHeight(40)
 
+    def init_awg_widgets(self):
+        self.awg_label = QLabel("AWG Control")
+        self.awg_label.setFixedHeight(40)
+        self.awg_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.awg_label.setFont(QFont(self.font, 20))
+
+        self.awg_volt_range_ch3_label = QLabel("AWG Ch. 3 Volt. Range: ")
+        self.awg_volt_range_ch3_label.setFont(QFont(self.font, 15))
+        self.awg_volt_range_ch4_label = QLabel("AWG Ch. 4 Volt. Range: ")
+        self.awg_volt_range_ch4_label.setFont(QFont(self.font, 15))
+        self.awg_samp_rate_group0_label = QLabel("AWG Group 0 Samp. Rate: ")
+        self.awg_samp_rate_group0_label.setFont(QFont(self.font, 15))
+        self.awg_samp_rate_group1_label = QLabel("AWG Group 1 Samp. Rate: ")
+        self.awg_samp_rate_group1_label.setFont(QFont(self.font, 15))
+        
+        awg_stylesheet = """
+        QComboBox {
+            border: 3px solid lightblue;
+            border-radius: 3px;
+            padding: 1px 18px 1px 3px;
+            min-width: 6em;
+        }
+
+        QComboBox::drop-down {
+            subcontrol-origin: padding;
+            subcontrol-position: top right;
+            width: 15px;
+            border-left-width: 1px;
+            border-left-color: black;
+            border-left-style: solid;
+            border-top-right-radius: 5px;
+            border-bottom-right-radius: 5px;
+        }
+
+        QComboBox QAbstractItemView {
+            border: 1px solid lightblue;
+            selection-background-color: lightblue;
+        }
+        """
+        # self.awg_volt_range_ch1_opts = ["0.2 V", "0.4 V", "0.6 V", "0.8 V", "1 V", "2 V", "3 V", "4 V", "5 V"]
+        # self.awg_volt_range_ch2_opts = ["0.2 V", "0.4 V", "0.6 V", "0.8 V", "1 V", "2 V", "3 V", "4 V", "5 V"]
+        self.awg_volt_range_ch3_opts = ["0.2 V", "0.4 V", "0.6 V", "0.8 V", "1 V", "2 V", "3 V", "4 V", "5 V"]
+        self.awg_volt_range_ch4_opts = ["0.2 V", "0.4 V", "0.6 V", "0.8 V", "1 V", "2 V", "3 V", "4 V", "5 V"]
+        self.awg_volt_range_ch3_combobox = QComboBox()
+        self.awg_volt_range_ch3_combobox.setStyleSheet(awg_stylesheet)
+        self.awg_volt_range_ch3_combobox.addItems(self.awg_volt_range_ch3_opts)
+        self.awg_volt_range_ch3_combobox.currentIndexChanged.connect(self.awg_volt_range_ch3_changed)
+        self.awg_volt_range_ch4_combobox = QComboBox()
+        self.awg_volt_range_ch4_combobox.setStyleSheet(awg_stylesheet)
+        self.awg_volt_range_ch4_combobox.addItems(self.awg_volt_range_ch4_opts)
+        self.awg_volt_range_ch4_combobox.currentIndexChanged.connect(self.awg_volt_range_ch4_changed)
+
+        self.apply_awg_volts_button = QPushButton("Apply Volt Ranges")
+        self.apply_awg_volts_button.setStyleSheet(awg_stylesheet)
+        self.apply_awg_volts_button.clicked.connect(self.apply_awg_volt_ranges)
+
+        self.awg_sampling_group0_opts = ["2.4 GHz", "1.2 GHz", "600 MHz", "300 MHz", "150 MHz", "75 MHz", "37.5 MHz", "18.75 MHz", "9.37 MHz", "4.68 MHz",
+                                  "2.34 MHz", "1.17 MHz", "585.93 kHz", "292.96 kHz"]
+        self.awg_sampling_group1_opts = ["2.4 GHz", "1.2 GHz", "600 MHz", "300 MHz", "150 MHz", "75 MHz", "37.5 MHz", "18.75 MHz", "9.37 MHz", "4.68 MHz",
+                                  "2.34 MHz", "1.17 MHz", "585.93 kHz", "292.96 kHz"]
+        self.awg_samp_rate_group_0_combobox = QComboBox()
+        self.awg_samp_rate_group_0_combobox.setStyleSheet(awg_stylesheet)
+        self.awg_samp_rate_group_0_combobox.addItems(self.awg_sampling_group0_opts)
+        self.awg_samp_rate_group_0_combobox.currentIndexChanged.connect(self.awg_samp_rate_group0_changed)
+        self.awg_samp_rate_group_1_combobox = QComboBox()
+        self.awg_samp_rate_group_1_combobox.setStyleSheet(awg_stylesheet)
+        self.awg_samp_rate_group_1_combobox.addItems(self.awg_sampling_group1_opts)
+        self.awg_samp_rate_group_1_combobox.currentIndexChanged.connect(self.awg_samp_rate_group1_changed)
+
+        self.awg_samp_rate_boxes = {
+            0: self.awg_samp_rate_group_0_combobox,
+            1: self.awg_samp_rate_group_1_combobox
+        }
+
+        self.apply_awg_rates_button = QPushButton("Apply Rates")
+        self.apply_awg_rates_button.setStyleSheet(awg_stylesheet)
+        self.apply_awg_rates_button.clicked.connect(self.apply_awg_rates)
+
+        self.awg_status_label = QLabel("AWG status here")
+        self.awg_status_label.setStyleSheet(
+            "color: white; background-color: black; border: 4px solid black;"
+        )
+        self.awg_status_label.setFixedHeight(40)
+
     def init_flipper_widgets(self):
         self.flipper_label = QLabel("Detector")
         self.flipper_label.setFixedHeight(40)
@@ -1108,6 +1193,30 @@ class InstWidget(QWidget):
         self.laser_controls_layout.addWidget(self.laser_b2, 4, 2, 1, 1)
         self.laser_controls_layout.addWidget(self.laser_shutter_button, 4, 3, 1, 1)
         self.laser_controls_layout.addWidget(self.laser_alarm_reset_button, 5, 1, 1, 3)
+        
+        self.awg_control_frame = QFrame(self)
+        self.awg_control_frame.setObjectName("awgControlFrame")
+        self.awg_control_frame.setStyleSheet(
+            "QFrame#deviceStatusFrame {"
+            "background-color: #3b1f0f;"      # dark brown background
+            "border: 2px solid #c68642;"      # warm tan / copper border
+            "border-radius: 5px;"
+            "}"
+        )
+        self.awg_control_layout = QGridLayout(self.awg_control_frame)
+        self.awg_control_layout.setSpacing(0)
+        self.awg_control_layout.addWidget(self.awg_label, 1, 1, 1, 2)
+        self.awg_control_layout.addWidget(self.awg_volt_range_ch3_label, 2, 1, 1, 1)
+        self.awg_control_layout.addWidget(self.awg_volt_range_ch3_combobox, 2, 2, 1, 1)
+        self.awg_control_layout.addWidget(self.awg_volt_range_ch4_label, 3, 1, 1, 1)
+        self.awg_control_layout.addWidget(self.awg_volt_range_ch4_combobox, 3, 2, 1, 1)
+        self.awg_control_layout.addWidget(self.apply_awg_volts_button, 4, 1, 1, 2)
+        self.awg_control_layout.addWidget(self.awg_samp_rate_group0_label, 5, 1, 1, 1)
+        self.awg_control_layout.addWidget(self.awg_samp_rate_group_0_combobox, 5, 2, 1, 1)
+        self.awg_control_layout.addWidget(self.awg_samp_rate_group1_label, 6, 1, 1, 1)
+        self.awg_control_layout.addWidget(self.awg_samp_rate_group_1_combobox, 6, 2, 1, 1)
+        self.awg_control_layout.addWidget(self.apply_awg_rates_button, 7, 1, 1, 2)
+        self.awg_control_layout.addWidget(self.awg_status_label, 8, 1, 1, 2)
 
         self.device_status_frame = QFrame(self)
         self.device_status_frame.setObjectName("deviceStatusFrame")
@@ -1150,9 +1259,9 @@ class InstWidget(QWidget):
 
         self.other_widgets_layout = QGridLayout()
         self.other_widgets_layout.addWidget(self.laser_control_frame, 1, 1, 1, 1)
-        self.other_widgets_layout.addWidget(self.detector_frame, 1, 2, 2, 1)
+        self.other_widgets_layout.addWidget(self.awg_control_frame, 1, 2, 1, 1)
         self.other_widgets_layout.addWidget(self.device_status_frame, 2, 1, 1, 1)
-
+        self.other_widgets_layout.addWidget(self.detector_frame, 2, 2, 1, 1)
 
         self.gui_layout.addLayout(self.individual_cmds_layout)
         self.gui_layout.addLayout(self.status_bar_layout)
@@ -1829,6 +1938,34 @@ class InstWidget(QWidget):
             "color: white; background-color: black; border: 4px solid black;"
         )
         self.sg396_status_label.setText("SRS SG396 Status: OFF")
+
+    def awg_volt_range_ch3_changed(self):
+        self.awg_volt_range_ch3_str = self.awg_volt_range_ch3_combobox.currentText()
+
+    def awg_volt_range_ch4_changed(self):
+        self.awg_volt_range_ch4_str = self.awg_volt_range_ch4_combobox.currentText()
+
+    def awg_samp_rate_group0_changed(self):
+        self.awg_samp_rate_group_0_str = self.awg_samp_rate_boxes[0].currentText()
+
+    def awg_samp_rate_group1_changed(self):
+        self.awg_samp_rate_group_1_str = self.awg_samp_rate_boxes[1].currentText()
+
+    def apply_awg_rates(self):
+        if hasattr(self, "awg_samp_rate_group_0_str") and hasattr(self, "awg_samp_rate_group_1_str"):
+            self.call_awg(lambda awg: awg.set_sampling_rate(0, self.awg_samp_rate_group_0_str))
+            self.call_awg(lambda awg: awg.set_sampling_rate(1, self.awg_samp_rate_group_1_str))
+            self.awg_status_label.setText(f"Applied AWG rates: Group 0 = {self.awg_samp_rate_group_0_str}, Group 1 = {self.awg_samp_rate_group_1_str}")
+        else:
+            self.awg_status_label.setText("Please select sampling rates for both groups before applying.")
+
+    def apply_awg_volt_ranges(self):
+        if hasattr(self, "awg_volt_range_ch3_str") and hasattr(self, "awg_volt_range_ch4_str"):
+            self.call_awg(lambda awg: awg.set_voltage_range(2, self.awg_volt_range_ch3_str))
+            self.call_awg(lambda awg: awg.set_voltage_range(3, self.awg_volt_range_ch4_str))
+            self.awg_status_label.setText(f"Applied AWG volt ranges: CH3 = {self.awg_volt_range_ch3_str}, CH4 = {self.awg_volt_range_ch4_str}")
+        else:
+            self.awg_status_label.setText("Please select voltage ranges for both channels before applying.")
 
     def laser_temp_clicked(self, component):
         match component:
