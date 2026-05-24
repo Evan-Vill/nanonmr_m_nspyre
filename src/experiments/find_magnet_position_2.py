2c9a383114b496ddebf18214558dfa5c75abced1
Evan Villafranca <ejvilla4@uchicago.edu>
2026-05-24 13:13:46 -0500

    Find magnet position for instrument GUI for new 2x2x2 magnet calibration

---
+++ src/experiments/find_magnet_position_2.py
@@ -0,0 +1,15 @@
+from scipy.optimize import curve_fit, fsolve
+import numpy as np
+
+def find_mag_pos(phi, b):
+    # file_path = 'C:\\Users\\ejvil\\nspyre\\src\\experiments\\all_b_field_fitted_params.csv'
+    file_path = 'C:/Users/nanon/nanon-nspyre/nanonmr_m_nspyre/src/experiments/all_b_field_fitted_params.csv'
+    data = np.loadtxt(file_path, delimiter=',')
+
+    mag_pos = fsolve(pos_solver, x0=10, args=(data, round(phi), float(b))) 
+    mag_pos = round(mag_pos[0], 2)
+
+    return mag_pos
+
+def pos_solver(x, data, azi, b):
+    return data[azi][0] * np.exp(-(data[azi][1]/x)**data[azi][2]) + data[azi][3] - float(b)
